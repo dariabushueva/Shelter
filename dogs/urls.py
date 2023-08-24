f944341990d98101edbbae29d28251eec6b5a52f
dariabushueva <bushueva84@yandex.ru>
2023-08-25 02:41:09 +0300

    added CRUD for app dogs

--- dogs/urls.py
+++ dogs/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 
 from dogs.apps import DogsConfig
-from dogs.views import index, categories, category_dogs
+from dogs.views import IndexView, CategoryListView, DogListView, DogCreateView, DogUpdateView, DogDeleteView
 
 app_name = DogsConfig.name
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('categories/', categories, name='categories'),
-    path('<int:pk>/dogs/', category_dogs, name='category_dogs')
+    path('', IndexView.as_view(), name='index'),
+    path('categories/', CategoryListView.as_view(), name='categories'),
+    path('<int:pk>/dogs/', DogListView.as_view(), name='category_dogs'),
+    path('create/', DogCreateView.as_view(), name='create_dog'),
+    path('edit/<int:pk>', DogUpdateView.as_view(), name='edit_dog'),
+    path('delete/<int:pk>/', DogDeleteView.as_view(), name='delete_dog')
 ]
